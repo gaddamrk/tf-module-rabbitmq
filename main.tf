73088d4e73da6e4a165a6cffd562122bcb5b144c
gaddamrk <raj>
2023-09-04 09:14:20 +0530

    jr

--- main.tf
+++ main.tf
@@ -26,14 +26,14 @@ resource "aws_security_group" "rabbitmq" {
   )
 }
 
-resource "aws_mq_configuration" "rabbitmq" {
-  description    = "${var.env}-rabbitmq-configration"
-  name           = "${var.env}-rabbitmq-configration"
-  engine_type    = var.engine_type
-  engine_version = var.engine_version
-
-  data           = ""
-}
+#resource "aws_mq_configuration" "rabbitmq" {
+#  description    = "${var.env}-rabbitmq-configration"
+#  name           = "${var.env}-rabbitmq-configration"
+#  engine_type    = var.engine_type
+#  engine_version = var.engine_version
+#
+#  data           = ""
+#}
 
 resource "aws_mq_broker" "rabbitmq" {
   broker_name = "${var.env}-rabbitmq"
@@ -43,19 +43,19 @@ resource "aws_mq_broker" "rabbitmq" {
   host_instance_type = var.host_instance_type
   security_groups    = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
 
-
-  configuration {
-    id       = aws_mq_configuration.rabbitmq.id
-    revision = aws_mq_configuration.rabbitmq.latest_revision
-  }
+#
+#  configuration {
+#    id       = aws_mq_configuration.rabbitmq.id
+#    revision = aws_mq_configuration.rabbitmq.latest_revision
+#  }
   encryption_options {
     use_aws_owned_key = false
   kms_key_id = data.aws_kms_key.key.arn
   }
 
   user {
-    username = data.aws_ssm_parameter.USER
-    password = data.aws_ssm_parameter.PASS
+    username = data.aws_ssm_parameter.USER.value
+    password = data.aws_ssm_parameter.PASS.value
   }
 }
 
